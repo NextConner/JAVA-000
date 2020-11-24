cb6b3ddc6217724ed00293a96ef9380e14fae486
NextConner <m13576046405@163.com>
2020-11-24 18:26:41 +0800

    add ignore and update sql file
    add ignore and update sql file

--- Week_06/commodity.sql
+++ Week_06/commodity.sql
@@ -1,53 +1,57 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : localhost_3306
-Source Server Version : 50712
-Source Host           : localhost:3306
-Source Database       : test
+ Source Server         : 本地库
+ Source Server Type    : MySQL
+ Source Server Version : 50727
+ Source Host           : localhost:3306
+ Source Schema         : yuedaovideo
 
-Target Server Type    : MYSQL
-Target Server Version : 50712
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50727
+ File Encoding         : 65001
 
-Date: 2020-11-24 08:29:12
+ Date: 24/11/2020 18:24:44
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for commodity
 -- ----------------------------
 DROP TABLE IF EXISTS `commodity`;
-CREATE TABLE `commodity` (
-  `id` bigint(32) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(64) COLLATE utf8_bin NOT NULL,
-  `sub_name` varchar(125) COLLATE utf8_bin NOT NULL,
-  `full_name` varchar(255) COLLATE utf8_bin NOT NULL,
-  `brand_id` bigint(16) NOT NULL,
-  `brand_info` varchar(255) COLLATE utf8_bin NOT NULL,
-  `sku_id` bigint(16) NOT NULL,
-  `sku_info` varchar(255) COLLATE utf8_bin NOT NULL,
-  `pre_price` int(8) NOT NULL,
-  `suf_price` double(2,0) NOT NULL DEFAULT '0',
-  `cover_img` varchar(255) COLLATE utf8_bin NOT NULL,
-  `imgs` text COLLATE utf8_bin,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE `commodity`  (
+  `id` bigint(32) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `first_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '商品名称',
+  `sub_name` varchar(125) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '商品附加名称',
+  `full_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '商品全称',
+  `brand_id` bigint(16) NOT NULL COMMENT '品牌ID',
+  `brand_info` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '品牌信息',
+  `sku_id` bigint(16) NOT NULL COMMENT 'skuID',
+  `sku_info` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT 'sku 信息',
+  `pre_price` int(8) NOT NULL COMMENT '价格整数部分',
+  `suf_price` double(2, 0) NOT NULL DEFAULT 0 COMMENT '价格小数部分',
+  `cover_img` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '商品封面图',
+  `imgs` text CHARACTER SET utf8 COLLATE utf8_bin NULL COMMENT '商品展示图',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `id` varchar(32) COLLATE utf8_bin NOT NULL,
-  `user_name` varchar(64) COLLATE utf8_bin NOT NULL,
-  `phone` bigint(13) unsigned NOT NULL,
-  `mail` varchar(32) COLLATE utf8_bin NOT NULL,
-  `password` varchar(64) COLLATE utf8_bin NOT NULL,
-  `default_address` varchar(255) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UNI_PHONE` (`phone`) USING BTREE,
-  UNIQUE KEY `UNI_MAIL` (`mail`) USING BTREE,
-  KEY `NAME_LOGIN_INDEX` (`user_name`,`password`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE `user`  (
+  `id` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '主键',
+  `user_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '用户名',
+  `phone` bigint(13) UNSIGNED NOT NULL COMMENT '手机号',
+  `mail` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '邮箱',
+  `password` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '密码',
+  `default_address` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '默认收货地址',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `UNI_PHONE`(`phone`) USING BTREE,
+  UNIQUE INDEX `UNI_MAIL`(`mail`) USING BTREE,
+  INDEX `NAME_LOGIN_INDEX`(`user_name`, `password`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
